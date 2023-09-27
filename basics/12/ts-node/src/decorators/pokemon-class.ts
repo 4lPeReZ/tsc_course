6d6978dbd760e262af1562a677e95edb49b4d152
4lPeReZ <alvaropr05@hotmail.com>
2023-09-27 10:42:25 +0200

    Update

--- basics/12/ts-node/src/decorators/pokemon-class.ts
+++ basics/12/ts-node/src/decorators/pokemon-class.ts
@@ -2,7 +2,17 @@ function printToConsole(constructor: Function) {
     console.log(constructor);
 }
 
-@printToConsole
+const printToConsoleConditional = (print: boolean = false):Function => {
+    if (print) {
+        return printToConsole;
+    }
+    return () => {};
+}
+
+// Decorator for classes
+/* @printToConsole */
+// Default decorator
+/* @printToConsoleConditional( true ) */
 
 export class Pokemon {
     public publicApi: string = 'https://pokeapi.co';
